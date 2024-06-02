8353d92a4108bad911d4650dd555c6f10b49cb10
Nana Kwame <iabu007@st.ug.edu.gh>
2024-06-02 19:33:08 +0000

    🚀initial commit -m with complete GradeCalculator program

---
+++ assignment-1/assignment-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GradeCalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the GradeCalculator class
+            GradeCalculator calculator = new GradeCalculator();
+
+            // Get the grade from the user
+            float grade = calculator.GetGradeFromUser();
+
+            // Determine the letter grade
+            string letterGrade = calculator.DetermineLetterGrade(grade);
+
+            // Display the letter grade
+            calculator.DisplayLetterGrade(letterGrade);
+        }
+    }
+}
